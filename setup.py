f746fbc3d10b1c1fccd71e6bc935f9bae7b36c4d
Intuitive Text Mining <42973185+intuitivetextmining@users.noreply.github.com>
2019-02-05 17:55:12 +0000

    ready for first pypi upload

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(name='d3fdgraph',
 	version='0.1',
-	description='Plots an interactive force directed graph in a jupyter notebook.',
-	url='https://github.com/intuitivetextmining/intuitivetextmining/d3fdgraph',
-	author='Tariq Rashid',
+	description='Interactive force-directed graph in a jupyter notebook.',
+	url='https://github.com/intuitivetextmining/d3fdgraph',
+	author='Intuitive Text Mining',
 	author_email='intuitivetextmining@gmail.com',
 	license='GPLv2',
 	packages=['d3fdgraph'],
-	install_requires=['random', 'IPython.core.display'],
+	install_requires=['IPython', 'networkx', 'networkx'],
 	include_package_data=True,
 	zip_safe=False)
