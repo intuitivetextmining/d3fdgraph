dcfc123f483d445f2d5e74948621704a3246f627
Intuitive Text Mining <42973185+intuitivetextmining@users.noreply.github.com>
2019-02-07 01:44:40 +0000

    readme.rst for long_description

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
+import pathlib
 from setuptools import setup
 
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.rst").read_text()
+
 setup(name='d3fdgraph',
-	version='0.2',
+	version='0.3',
 	description='Interactive force-directed graph in a jupyter notebook.',
+	long_description=README,
+    long_description_content_type="text/x-rst",
 	url='https://github.com/intuitivetextmining/d3fdgraph',
 	author='Intuitive Text Mining',
 	author_email='intuitivetextmining@gmail.com',
